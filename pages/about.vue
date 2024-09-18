728b9cb5892ca9a945358d133ba77d34389096b1
Chrisinii <christina.benz@stud.fhgr.ch>
2024-09-18 22:03:40 +0200

    .

--- pages/about.vue
+++ pages/about.vue
@@ -25,22 +25,22 @@ import { ref } from 'vue'
 const items = ref([
     {
         question: 'What is this website about?',
-        answer: 'Welcome to my climate change news project! This site is part of my final coursework for my Multimedia Production bachelor’s degree at the University of Applied Sciences of the Grisons. The idea is to give you a glimpse into how climate change is reported across different European countries. We pull in news articles about climate change using the NewsData.io API, so you get a broad view of how various news organizations cover this important topic.<br><br>All the articles you see here are focused on climate change and are gathered based on specific keywords. If you\'re curious about which news sources we\'re tracking in each country, check out this link: <a href="https://newsdata.io/news-sources" target="_blank" style="text-decoration: underline" class="news-link">www.NewsData.io/NewsSources</a>.',
+        answer: 'Welcome to my climate change news project! This site is part of my final coursework for my Multimedia Production bachelor’s degree at the University of Applied Sciences of the Grisons. The idea is to give you a glimpse into how climate change is reported across different European countries. We pull in news articles about climate change using the NewsData.io API, so you get a broad view of how various news organizations cover this important topic.<br><br>All the articles you see here are focused on climate change and are gathered based on specific keywords. You can find the keywords on <a href="https://github.com/Chrisinii/europe-climate-reporting" target="_blank" style="text-decoration: underline" class="news-link">my GitHub Repository</a>.',
         open: false
     },
     {
         question: 'What technologies are used?',
-        answer: 'Our website uses a bunch of technologies to bring you the latest climate change news:<br><br> <ul><li>NewsData.io: This is where we fetch all our climate news.<br>Microsoft Azure Translator: We use this to translate articles from their original language into English.</li><li>AWS Lambda and EventBridge: These tools help us make automatic API requests every few minutes.</li><li>Supabase: We use this to store our data securely.</li><li>Nuxt3.js and Tailwind: These frameworks make our frontend look great and work smoothly.</li></ul>',
+        answer: 'The website uses a bunch of technologies to bring you the latest climate change news:<br><br> <ul><li>NewsData.io: This is where all the  climate news are fetched.<br>Microsoft Azure Translator: Is used to translate the articles from their original language into English.</li><li>AWS Lambda and EventBridge: These tools make automatic API requests every five minutes.</li><li>Supabase: Used to store the data securely.</li><li>Nuxt3.js and Tailwind: These frameworks make the frontend look great and work smoothly.</li></ul> <br>If you want to learn more about the code, feel free to chech out <a href="https://github.com/Chrisinii/europe-climate-reporting" target="_blank" style="text-decoration: underline" class="news-link">my GitHub Repository</a>.',
         open: false
     },
     {
         question: 'Which news sites get tracked?',
-        answer: 'We track a variety of news sites from across Europe to give you a comprehensive view of climate change reporting. If you want to see exactly which news outlets we\'re pulling from in any given country, you can check out this link: <a href="https://newsdata.io/news-sources" target="_blank" style="text-decoration: underline" class="news-link">www.NewsData.io/NewsSources</a>.',
+        answer: 'A variety of news sites from across Europe are tracked to give you a comprehensive view of climate change reporting. If you want to see exactly which news outlets get tracked, you can check out this link: <a href="https://newsdata.io/news-sources" target="_blank" style="text-decoration: underline" class="news-link">www.NewsData.io/NewsSources</a>.',
         open: false
     },
     {
         question: 'How often is the website updated?',
-        answer: 'We keep things fresh and up-to-date! The site automatically pulls in new news articles from one country every 5 minutes between 09:00 and 21:00. This means each country’s news is refreshed every three hours during this period.',
+        answer: 'The articles are up-to-date! The site automatically pulls in new news articles from one country every 5 minutes between 09:00 and 21:00. This means each country’s news is refreshed every three hours during this period.',
         open: false
     }
 ])
